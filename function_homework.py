5baa95cdefc7770a32fcfdc79511434a394ab31b
Morven <morvendouglas-@hotmail.co.uk>
2021-08-31 20:41:17 +0100

    updated functions homework

--- function_homework.py
+++ function_homework.py
@@ -1,66 +1,45 @@
-# def greet():
-#     print("hey")
+def greet():
+    print("hey")
 
-# greet()
+greet()
 
-# def greet():
-#     return "hey"
 
-# greeting = greet()
-# print(greeting)
+def greet():
+    return "hey"
 
-# def greet():
-#     print("hey")
+greeting = greet()
+print(greeting)
 
-# greeting = greet()
-# print(greeting)
 
-# def greet(name):
-#     return "Hey " + name
+def greet(name):
+    return "Hey " + name
 
-# greeting = greet("Morven")
-# print(greeting)
+greeting = greet("Morven")
+print(greeting)
 
-# def greet(name, time_of_day):
-#     return "Good " + time_of_day ", " + name
 
-# greeting = greet('Bob', "morning")
-# print(greeting)
+def greet(name, time_of_day):
+    return "Good " + time_of_day + ", " + name
 
-# my_functions.py
+greeting = greet('Bob', "morning")
+print(greeting)
 
-# def greet(name, time_of_day):
-#   return "Good " + time_of_day + ", " + name
 
-# name_1 = "Bob"
-# time_of_day_1 = "morning"
-# greeting = greet(name_1, time_of_day_1)
-# print(greeting)
 
-# name_2 = "Ada"
-# time_of_day_2 = "afternoon"
-# greeting = greet(name_2, time_of_day_2)
-# print(greeting)
+def greet(name, time_of_day):
+  return "Good " + time_of_day + ", " + name
 
-# my_functions.py
+name_1 = "John"
+time_of_day_1 = "afternoon"
+greeting = greet(name_1, time_of_day_1)
+print(greeting)
 
-# chickens = [
-#   { "name": "Margaret", "age": 2, "eggs": 0 },
-#   { "name": "Hetty", "age": 1, "eggs": 2 },
-#   { "name": "Henrietta", "age": 3, "eggs": 1 },
-#   { "name": "Audrey", "age": 2, "eggs": 0 },
-#   { "name": "Mabel", "age": 5, "eggs": 1 },
-# ]
+name_2 = "Amy"
+time_of_day_2 = "evening"
+greeting = greet(name_2, time_of_day_2)
+print(greeting)
 
-# total_eggs = 0
 
-# for chicken in chickens:
-#     total_eggs += chicken["eggs"]
-#     chicken["eggs"] = 0
-
-# print(f"{total_eggs} eggs collected")
-
-# my_functions.py
 
 chickens = [
   { "name": "Margaret", "age": 2, "eggs": 0 },
@@ -70,25 +49,37 @@ chickens = [
   { "name": "Mabel", "age": 5, "eggs": 1 },
 ]
 
-# def count_eggs( list ):
-#     total_eggs = 0
+total_eggs = 0
 
-#     for bird in list:
-#         total_eggs += bird["eggs"]
-#         bird["eggs"] = 0 # eggs have been collected
+for chicken in chickens:
+    total_eggs += chicken["eggs"]
+    chicken["eggs"] = 0
 
-#     return f"{total_eggs} eggs collected"
+print(f"{total_eggs} eggs collected")
 
-# print(count_eggs(chickens))
 
-# def count_eggs( list ):
-#     total_eggs = 0
+chickens = [
+  { "name": "Margaret", "age": 2, "eggs": 0 },
+  { "name": "Hetty", "age": 1, "eggs": 2 },
+  { "name": "Henrietta", "age": 3, "eggs": 1 },
+  { "name": "Audrey", "age": 2, "eggs": 0 },
+  { "name": "Mabel", "age": 5, "eggs": 1 },
+]
 
-#     for bird in list:
-#         total_eggs += bird["eggs"]
-#         bird["eggs"] = 0
 
-#     return f"{total_eggs} eggs collected"
+def count_eggs( list ):
+    total_eggs = 0
+
+    for bird in list:
+        total_eggs += bird["eggs"]
+        bird["eggs"] = 0
+
+    return f"{total_eggs} eggs collected"
+
+print(count_eggs(chickens))
+
+
+
 
 
 
